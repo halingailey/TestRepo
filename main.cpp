802fb9509a37d1de5dfb95694db16a06a2b824a1
Halin Gailey <hgailey@csuchico.edu>
2025-02-18 21:16:51 -0800

    [added new print lines to main.cpp]

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 #include <iostream>
 
 int main(){
+  cout << "confused" << endl;
+  cout << "gailey << endl;
+  cout << "halin << endl;
   cout << "hello" << endl;
   cout << "world" << endl;
   cout << "!" << endl;
