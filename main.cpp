0a082f4500d3761b44f50e4b39276076fe848476
halingailey <halin1221@yahoo.com>
2025-02-18 19:57:14 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+#include <vector>
+#include <string>
+#include <iostream>
+
+int main(){
+  cout << "hello" << endl;
+  cout << "world" << endl;
+  cout << "!" << endl;
+  return 0;
+}
